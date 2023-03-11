e3a0321082e0b174dc97ff718f67e03e2c553701
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-03-11 18:33:40 +0200

    Update PatientService.java

--- src/main/java/com.mahmoud/service/PatientService.java
+++ src/main/java/com.mahmoud/service/PatientService.java
@@ -1,11 +1,18 @@
 package com.mahmoud.service;
 
 import com.mahmoud.entity.Patient;
+import com.mahmoud.mapper.PatientRegisterScreenOneDto;
 import com.mahmoud.request.CreatePatientProfileRequest;
 import com.mahmoud.response.CreatePatientResponse;
+import com.mahmoud.response.SuccessResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface PatientService {
 
-    ResponseEntity<CreatePatientResponse> insert(CreatePatientProfileRequest createPatientProfileRequest);
-}
+    ResponseEntity<CreatePatientResponse> create(CreatePatientProfileRequest createPatientProfileRequest);
+
+    Patient getPatientById(Long id);
+
+    SuccessResponse<Patient> delete(Long id);
+
+    SuccessResponse<Patient> update(Long id, PatientRegisterScreenOneDto dto);}
